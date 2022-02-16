8da1d5f8f698224bcf5c715908c948f1252d7f30
Mirek Ostafinski <ostafinskim@gmail.com>
2022-02-16 21:30:42 +0000

    feat: table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Product } from '../../types/table';
+import CustomTable from './CustomTable';
+
+const initialState = [
+  {
+    id: 1,
+    title: 'First Product',
+    price: 10000,
+    description: 'First Product description...',
+    published: true,
+  },
+  {
+    id: 2,
+    title: 'Second Product',
+    price: 2000,
+    description: 'Second Product description...',
+    published: false,
+  },
+  {
+    id: 3,
+    title: 'Third Product',
+    price: 3000,
+    description: 'Third Product description...',
+    published: true,
+  },
+];
 
 const Table = () => {
-  return (
-    <section>
-      <h1>Table</h1>
-    </section>
-  );
+  const [products] = useState<Product[]>(initialState);
+  return <CustomTable data={products} />;
 };
 
 export default Table;
