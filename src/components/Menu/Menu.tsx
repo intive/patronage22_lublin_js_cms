305cd7e62c247eb51a3a84dc1c934bff3e3c1339
Krzysztof Stankiewicz <krzystan@wp.pl>
2022-02-16 18:43:02 +0100

    feat: add navigation with Drawer-mui

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import PermanentDrawerLeft from "../Navigation/Navigation";
 
 const Menu = () => {
-  return (
-    <section>
-      <h1>Menu</h1>
-    </section>
-  );
+    return (
+        <section >
+            <h1 >Menu</h1 >
+            <PermanentDrawerLeft />
+        </section >
+    );
 };
-
 export default Menu;
--- src/types/routes.ts
+++ src/types/routes.ts
@@ -2,3 +2,14 @@ export enum ROUTES {
     AUTH = '/',
     DASHBOARD = '/dashboard'
 }
+
+const menuLinks = [
+    { id:1, to: '/dashboard', text: 'Home'},
+    { id:2, to: '/pages', text: 'Strony'},
+    { id:3, to: '/category', text: 'Kategorie'},
+    { id:4, to: '/products', text: 'Produkty'},
+    { id:5, to: '/orders', text: 'Zamówienia'},
+    { id:6, to: '/clients', text: 'Klienci'},
+];
+
+export default menuLinks;
