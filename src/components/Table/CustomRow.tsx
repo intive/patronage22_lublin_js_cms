d5497c58b14fcf4c6ddc9ab721596b27bd83e89c
Mirek Ostafinski <ostafinskim@gmail.com>
2022-02-16 21:30:06 +0000

    feat: custom table row component

---
+++ src/components/Table/CustomRow.tsx
@@ -0,0 +1,33 @@
+import { Chip } from '@mui/material';
+import * as React from 'react';
+import { CustomRowProps } from '../../types/table';
+import { StyledTableCell, StyledTableRow } from './styles';
+
+const CustomRow: React.FC<CustomRowProps> = ({ product }) => {
+  const { id, title, price, description, published } = product;
+  return (
+    <StyledTableRow>
+      <StyledTableCell component="th" scope="row" align="center">
+        {id}
+      </StyledTableCell>
+      <StyledTableCell align="center">{title}</StyledTableCell>
+      <StyledTableCell align="center">
+        <Chip
+          clickable
+          label={`${price.toLocaleString()} $`}
+          variant="filled"
+        />
+      </StyledTableCell>
+      <StyledTableCell align="center">{description}</StyledTableCell>
+      <StyledTableCell align="center">
+        {published ? (
+          <Chip clickable label="Yes" color="success" variant="filled" />
+        ) : (
+          <Chip clickable label="No" color="error" variant="filled" />
+        )}
+      </StyledTableCell>
+    </StyledTableRow>
+  );
+};
+
+export default CustomRow;
