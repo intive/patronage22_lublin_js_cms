0e1ac57291db1ce7c96a33b432f68a841cb8082e
Mirek Ostafinski <ostafinskim@gmail.com>
2022-02-16 21:29:15 +0000

    feat: custom table elements styles

---
+++ src/components/Table/styles.ts
@@ -0,0 +1,26 @@
+import { TableSortLabel } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+
+export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+}));
+
+export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+export const StyledTableSortLabel = styled(TableSortLabel)(({ theme }) => ({
+  [`&.Mui-active`]: {
+    color: theme.palette.common.white,
+  },
+}));
