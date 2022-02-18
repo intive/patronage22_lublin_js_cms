715165102b5ca9f8fda01340765859025d9d7da8
Krzysztof Stankiewicz <krzystan@wp.pl>
2022-02-18 17:15:07 +0100

    feat: after meeting with client

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import NavItemList from "./NavItemList";
+import {Box, CssBaseline, List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import menuLinks from "../../types/routes";
+import {Link as RouterLink} from 'react-router-dom';
 
+type NavProps = {
+  label: string,
+  id: number,
+  to: string
+}
 export default function Navigation() {
   return (
       <Box sx={{display: 'flex'}}>
         <CssBaseline/>
-        <NavItemList/>
+        <List>
+          {menuLinks.map((props: NavProps) => (
+              <ListItem button component={RouterLink} to={props.to} key={props.label}>
+                <ListItemIcon>
+                  {props.id % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                </ListItemIcon>
+                <ListItemText primary={props.label}/>
+              </ListItem>
+          ))}
+        </List>
       </Box>
   );
 }
