45c9d75a62639ef258865b27dbafc172cd12d2e4
Kacper Stoczkowski <kacper.stoczkowski@gmail.com>
2022-02-21 15:02:33 +0100

    feat: Implemented submitHandler

--- src/components/EditCatForm/EditCatForm.tsx
+++ src/components/EditCatForm/EditCatForm.tsx
@@ -1,29 +1,41 @@
 import React from "react";
-import { Formik } from "formik";
+import { useRef } from "react"; 
+import { Formik, Form } from "formik";
 import { Checkbox, FormControlLabel, Button, TextField } from "@material-ui/core";
 
-const EditCatForm = () => {
+const EditCatForm = (props: any) => {
+    const titleRef useRef();
+    
+    const submitHandler = (event: any) => {
+        event.preventDefault();
+
+        const editedTitle = titleRef.current.value;
+    }
+
     return (
         <Formik
-            initialValues={{title: ''}}
+            initialValues={{title: props.title}}
+
             onSubmit={(values, actions) => {
                 console.log({ values, actions });
                 alert(JSON.stringify(values, null, 2));
                 actions.setSubmitting(false);
             }}
         >
-            <TextField id="outlined-basic" label="New Title" variant="outlined" />
-            
-            <FormControlLabel
-                value="bottom"
-                control={<Checkbox />}
-                label="Published"
-                labelPlacement="bottom"
-            />
+            <Form onSubmit={submitHandler}>
+                <TextField id="outlined-basic" label="New Title" variant="outlined" name="title" required ref={titleRef}/>
+                
+                <FormControlLabel
+                    value="bottom"
+                    control={<Checkbox />} 
+                    label="Published"
+                    labelPlacement="bottom"
+                />
 
-            <Button variant="contained">Add</Button>
+                <Button variant="contained">Add</Button>
+            </Form>
         </Formik>
     )
 }
-
+ 
 export default EditCatForm;
