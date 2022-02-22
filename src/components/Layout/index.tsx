b81fc30509b91fab060312db15f32f4e08c1757c
Krzysztof Stankiewicz <krzystan@wp.pl>
2022-02-22 13:20:28 +0100

    fix: after code review

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Dashboard from "../../pages/Dashboard";
-import Menu from "../Menu/Menu";
+import Menu from "../Menu";
 import Header from "../Header/Header";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
