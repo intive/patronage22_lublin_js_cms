91dbfefeb7da45a665dce40d2a98f048d68f28a6
Kacper Stoczkowski <kacper.stoczkowski@gmail.com>
2022-02-22 18:58:51 +0100

    fix: Changed validation method

--- src/components/EditCategoryForm/index.tsx
+++ src/components/EditCategoryForm/index.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import { useRef } from "react"; 
-import { Formik, Form } from "formik";
+import { Form, useFormik , Formik } from "formik";
 import { Checkbox, FormControlLabel, Button, TextField } from "@material-ui/core";
 import * as Yup from "yup";
 
 const EditCatForm = (props: String) => {
-    // const titleRef useRef();
-    
-    // const submitHandler = (event: { preventDefault: () => void; }) => {
-    //     const editedTitle = titleRef.current.value;
-    // }
 
     const EditCategoryValidation = Yup.object().shape({
         title: Yup.string()
@@ -18,38 +12,73 @@ const EditCatForm = (props: String) => {
         .matches(/^[aA-zZ\s]+$/, "Only alphabets are allowed for this field")
     });
 
-    return (
-        <Formik
-            initialValues={{title: props.title}}
-
-            onSubmit={(values, actions) => {
+    const EditCategoryForm = () => {
+        const formik = useFormik({
+            initialValues: { title: {props.title} },
+            
+            onSubmit: (values, actions) => {
                 console.log({ values, actions });
                 alert(JSON.stringify(values, null, 2));
                 actions.setSubmitting(false);
-            }}
+            },
 
-            validationSchema = {EditCategoryValidation}
-        >
-            <Form onSubmit={submitHandler}>
-                <TextField 
-                    id="outlined-basic" 
-                    label="New Title" 
-                    variant="outlined" 
-                    name="title" 
-                    required 
-                    ref={titleRef}
-                />
+            validationSchema: { EditCategoryValidation }
+        })
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <TextField 
+                id="outlined-basic" 
+                label="New Title" 
+                variant="outlined" 
+                name="title" 
+                onChange={handleChange}
+                required
+            />
+
+            <FormControlLabel
+                value="bottom"
+                control={<Checkbox />} 
+                label="Published"
+                labelPlacement="bottom"
+                onChange={handleChange}
+            />
+
+            <Button variant="contained" onChange={handleChange}>Add</Button>
+        </Form>
+
+        // <Formik
+        //     initialValues={{title: props.title}}
+
+        //     onSubmit={(values, actions) => {
+        //         console.log({ values, actions });
+        //         alert(JSON.stringify(values, null, 2));
+        //         actions.setSubmitting(false);
+        //     }}
+
+        //     validationSchema = {EditCategoryValidation}
+        // >
+        //     <Form onSubmit={submitHandler}>
+        //         <TextField 
+        //             id="outlined-basic" 
+        //             label="New Title" 
+        //             variant="outlined" 
+        //             name="title" 
+        //             required 
+        //             ref={titleRef}
+        //         />
                 
-                <FormControlLabel
-                    value="bottom"
-                    control={<Checkbox />} 
-                    label="Published"
-                    labelPlacement="bottom"
-                />
+        //         <FormControlLabel
+        //             value="bottom"
+        //             control={<Checkbox />} 
+        //             label="Published"
+        //             labelPlacement="bottom"
+        //         />
 
-                <Button variant="contained">Add</Button>
-            </Form>
-        </Formik>
+        //         <Button variant="contained">Add</Button>
+        //     </Form>
+        // </Formik>
     )
 }
  
