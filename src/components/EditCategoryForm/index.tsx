19cd99450945e3b7a04cc94a5d4dc5c0f7ca65ab
Kacper Stoczkowski <kacper.stoczkowski@gmail.com>
2022-03-07 18:36:16 +0100

    fix: Changed TexField to TextfieldWrapper nad useFormik to Formik

--- src/components/EditCategoryForm/index.tsx
+++ src/components/EditCategoryForm/index.tsx
@@ -1,47 +1,50 @@
 import React from "react";
-import { Form, useFormik }  from "formik";
-import { Checkbox, FormControlLabel, Button, TextField } from "@material-ui/core";
 import EditCategoryValidation from "./validation";
+import TextfieldWrapper from "../Textfield/TextfieldWrapper";
+import { Form, useFormik, Formik, FormikProps }  from "formik";
+import { Checkbox, FormControlLabel, Button } from "@material-ui/core";
 
 type FormProps = {
-    id: String
+    id: String;
 }
  
 const EditCategoryForm = ({ id }: FormProps) => {
-        const initialFormValues = {title: String};
+        const initialFormValues = {
+            title: '',
+            published: true
+        };
 
-        const formik = useFormik({
-            initialValues: {initialFormValues},
-             
-            onSubmit: (values, actions) => {
-                console.log({ values, actions });
-                alert(JSON.stringify(values, null, 2));
-                actions.setSubmitting(false);
-            },
-
-            validationSchema: { EditCategoryValidation }
-        })
-
-        const { handleSubmit, handleChange } = formik;
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <TextField 
-                {...formik.getFieldProps('title')}
-                label="New Title" 
-                variant="outlined" 
-            />
-
-            <FormControlLabel
-                {...formik.getFieldProps('button')}
-                control={<Checkbox />} 
-                label="Published"
-                labelPlacement="bottom"
-            />
-
-            <Button variant="contained" onChange={handleChange}>Edit</Button>
-        </form>
-    )
+        return (
+            <Formik
+                initialValues={initialFormValues}
+                onSubmit={(values, actions) => {
+                    console.log({ values, actions });
+                    alert(JSON.stringify(values, null, 2));
+                    actions.setSubmitting(false);
+                }}
+                validationSchema={EditCategoryValidation}
+            >
+                {(props: FormikProps<{title: string}>) => (
+                    <Form onSubmit={props.handleSubmit}>
+                        <TextfieldWrapper
+                            {...props.getFieldProps('title')}
+                            label="New Title"
+                            name="title"
+                        />
+                
+                        <FormControlLabel
+                            {...props.getFieldProps('button')}
+                            control={<Checkbox />} 
+                            label="Published"
+                            labelPlacement="bottom"
+                            name="published"
+                        />
+                
+                        <Button variant="contained" type="submit">Edit</Button>
+                    </Form> 
+                )}
+            </Formik>
+        )
 }
  
 export default EditCategoryForm;
