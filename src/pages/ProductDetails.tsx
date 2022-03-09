3d24fcbf967f69ab38c4a3f8c86932c1aafd616d
JakubSoldek <j.soldek@gmail.com>
2022-03-09 14:58:50 +0100

    feat: add category values from API

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,31 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { EditProductForm } from "../components/EditProductForm/EditProductForm";
+import getCategories from "../components/lib/categories";
+
+interface MyCategories {
+  id: number;
+  title: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
 const ProductDetails: React.FC = () => {
-  const categories = [
-    {
-      id: 3,
-      title: "Electronics",
-      description: "Electronics products",
-      createdAt: "2022-02-16T09:36:41.000Z",
-      updatedAt: "2022-02-16T09:36:41.000Z",
-    },
-    {
-      id: 4,
-      title: "Clothing",
-      description: "Clothing products",
-      createdAt: "2022-02-16T09:58:06.000Z",
-      updatedAt: "2022-02-16T09:58:06.000Z",
-    },
-    {
-      id: 5,
-      title: "Books",
-      description: "Enjoy reading books",
-      createdAt: "2022-02-16T21:04:55.000Z",
-      updatedAt: "2022-02-16T21:17:35.000Z",
-    },
-  ];
+  const [categories, setCategories] = useState<MyCategories[]>([]);
 
+  useEffect(() => {
+    getCategories()
+      .then((response) => {
+        // console.log(response);
+        const data = response.data;
+        setCategories(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  
   const product = {
     id: 1,
     title: "First Product",
