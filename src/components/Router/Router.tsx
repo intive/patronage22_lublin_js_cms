dced1effcc4b161046385bf3d26aeb01b96aeba3
unknown <dmytro.tarasiuk1899@gmail.com>
2022-04-02 23:49:24 +0200

    bug-1643: multiple logins allowed

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,25 +1,38 @@
-import React from "react";
-import {Switch, Route} from "react-router-dom";
+import React, { useContext } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import AuthLayout from "../Layout/AuthLayout";
 import Layout from "../Layout";
-import {ROUTES} from "../../types/routes";
+import { ROUTES } from "../../types/routes";
 import AuthGuard from "../HOC/AuthGuard";
+import AuthContext from "../../store/auth-context";
 
 const Router = () => {
+  const authCtx = useContext(AuthContext);
+
   return (
-      <Switch>
-        <Route exact path={ROUTES.AUTH} component={AuthLayout}/>
-        <Route exact path={ROUTES.ADD_PRODUCT} component={AuthGuard(Layout)} />
-        <Route exact path={ROUTES.DASHBOARD} component={AuthGuard(Layout)}/>
-        <Route exact path={ROUTES.PAGES} component={AuthGuard(Layout)}/>
-        <Route exact path={ROUTES.PRODUCT} component={AuthGuard(Layout)} />
-        <Route exact path={ROUTES.PRODUCTS} component={AuthGuard(Layout)} />
-        <Route exact path={ROUTES.PRODUCT_DETAILS} component={AuthGuard(Layout)} />
-        <Route exact path={ROUTES.CATEGORY_ADD} component={AuthGuard(Layout)} />
-        <Route exact path={ROUTES.CATEGORIES} component={AuthGuard(Layout)} />
-        <Route exact path={ROUTES.PAGE_ADD} component={AuthGuard(Layout)} />
-        <Route component={AuthGuard(Layout)}/>
-      </Switch>
+    <Switch>
+      {!authCtx.isLoggedIn ? (
+        <Route exact path={ROUTES.AUTH} component={AuthLayout} />
+      ) : (
+        <Route path={ROUTES.AUTH} exact>
+          <Redirect to={ROUTES.DASHBOARD} />
+        </Route>
+      )}
+      <Route exact path={ROUTES.ADD_PRODUCT} component={AuthGuard(Layout)} />
+      <Route exact path={ROUTES.DASHBOARD} component={AuthGuard(Layout)} />
+      <Route exact path={ROUTES.PAGES} component={AuthGuard(Layout)} />
+      <Route exact path={ROUTES.PRODUCT} component={AuthGuard(Layout)} />
+      <Route exact path={ROUTES.PRODUCTS} component={AuthGuard(Layout)} />
+      <Route
+        exact
+        path={ROUTES.PRODUCT_DETAILS}
+        component={AuthGuard(Layout)}
+      />
+      <Route exact path={ROUTES.CATEGORY_ADD} component={AuthGuard(Layout)} />
+      <Route exact path={ROUTES.CATEGORIES} component={AuthGuard(Layout)} />
+      <Route exact path={ROUTES.PAGE_ADD} component={AuthGuard(Layout)} />
+      <Route component={AuthGuard(Layout)} />
+    </Switch>
   );
 };
 export default Router;
