9ce7818864d74b81fef2cab693df03d327f9a4ed
Kamil <kamilszalast0@gmail.com>
2022-04-09 17:35:49 +0200

    fix: fixed column width of title to 20%

--- src/components/Table/CategoryRow/index.tsx
+++ src/components/Table/CategoryRow/index.tsx
@@ -1,18 +1,20 @@
-import {Chip} from "@mui/material";
+import { Chip } from "@mui/material";
 import React from "react";
-import {CustomRowProps} from "../../../types/table";
-import {StyledTableCell, StyledTableRow} from "../styles";
+import { CustomRowProps } from "../../../types/table";
+import { StyledTableCell, StyledTableRow } from "../styles";
 import EditButton from "../../EditButton";
-import {parseISO, format} from "date-fns";
+import { parseISO, format } from "date-fns";
 
-const CategoryRow: React.FC<CustomRowProps> = ({row}) => {
-  const {id, title, published, createdAt} = row;
+const CategoryRow: React.FC<CustomRowProps> = ({ row }) => {
+  const { id, title, published, createdAt } = row;
   return (
     <StyledTableRow>
       <StyledTableCell component='th' scope='row' align='center'>
         {id}
       </StyledTableCell>
-      <StyledTableCell align='center'>{title}</StyledTableCell>
+      <StyledTableCell align='center' width='20%'>
+        {title}
+      </StyledTableCell>
       <StyledTableCell align='center'>
         {published ? (
           <Chip clickable label='Yes' color='success' variant='filled' />
